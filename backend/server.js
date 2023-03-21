7b715fec13040e40d0f1ac084fdf366554b45017
ciscomar <cisco.morales.27@gmail.com>
2023-03-21 12:51:57 -0600

    init

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,19 @@ require('colors')
 require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const cors = require('cors');
 const PORT = process.env.PORT || 5000
 
 // Connect to database
 connectDB()
 
 const app = express()
+app.use(cors());
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
